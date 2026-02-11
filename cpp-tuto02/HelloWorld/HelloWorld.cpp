bf8738c765aabb5112205c248a39e32869d41c93
Laurent Larsonneur <laurent.larsonneur@laposte.net>
2026-02-11 08:15:22 +0100

    Tutorial 2 - Approfondissons le C++

---
+++ cpp-tuto02/HelloWorld/HelloWorld.cpp
@@ -0,0 +1,12 @@
+// Inspiré de la Playlist Youtube "C++" de The Cherno
+// [Welcome to C++](https://youtu.be/18c3MTX0PK0)
+// [How to Setup C++ on Windows](https://youtu.be/1OsGXuNA5cc)
+// LL - 11.02.2026
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!" << std::endl;
+    std::cin.get();
+}
